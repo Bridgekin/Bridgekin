354ee2232dc363c772c7290f7875632cde8f9f8c
Eric <eaconwayjr@gmail.com>
2019-01-11 12:35:01 -0800

    Update activeadmin params

--- app/admin/user_networks.rb
+++ app/admin/user_networks.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register UserNetwork do
+  permit_params :network_id, :member_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
