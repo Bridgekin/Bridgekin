4ea76c86ccd73ce1528cbc2e5af3c727f3da92e1
Eric <eaconwayjr@gmail.com>
2019-03-13 15:00:22 -0700

    Update sql queries for specific networks

--- app/controllers/concerns/sql_controller_module.rb
+++ app/controllers/concerns/sql_controller_module.rb
@@ -32,6 +32,7 @@ module SQLControllerModule
       .where(status: 'Approved')
       .where.not(deal_status: 'Deleted')
       .order(created_at: :desc)
+
     all = Opportunity.joins("INNER JOIN opp_permissions on opp_permissions.opportunity_id = opportunities.id AND opp_permissions.shareable_type = 'Connection'")
       .includes(:owner)
       .where(opp_permissions: { shareable_id: nil })
@@ -41,7 +42,7 @@ module SQLControllerModule
   end
 
   def opps_network_id(network_id)
-    Network.find(network_id).opportunities
+    network_opps = Network.find(network_id).opportunities
       .includes(:owner)
       .where(status: 'Approved')
       .where.not(deal_status: 'Deleted')
@@ -49,5 +50,14 @@ module SQLControllerModule
       # .where(opp_permissions: { sharable_id: network_id,
       #   shareable_type: 'Network' })
       # .where(opportunity_networks: { network_id: network_id})
+
+    all_opps = Opportunity.joins("INNER JOIN opp_permissions on opp_permissions.opportunity_id = opportunities.id AND opp_permissions.shareable_type = 'Network'")
+      .includes(:owner)
+      .where(opp_permissions: { shareable_id: [@workspace_networks.pluck(:id), nil]})
+      .where(status: 'Approved')
+      .where.not(deal_status: 'Deleted')
+      .order(created_at: :desc)
+
+    network_opps + all_opps
   end
 end
