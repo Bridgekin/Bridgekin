ad9a5ada43f18fa189a320e332db1dbf2a425fec
E.J <eaconwayjr@gmail.com>
2019-06-14 16:32:40 -0700

    Update migrations

--- db/migrate/20190614232219_create_sales_network.rb
+++ db/migrate/20190614232219_create_sales_network.rb
@@ -1,6 +1,10 @@
 class CreateSalesNetwork < ActiveRecord::Migration[5.2]
   def change
     create_table :sales_networks do |t|
+      t.string :title, default: ''
+      t.string :domain, default: ''
+
+      t.timestamps
     end
   end
 end
